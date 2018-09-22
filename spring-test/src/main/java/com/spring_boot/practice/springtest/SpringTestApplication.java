0cb8bf0246f9a6d5e8b4859a8688c4d2a9643be9
haixiaoxuan <15735185585@163.com>
2018-09-22 11:10:49 +0800

    create a new protect for practice

---
+++ spring-test/src/main/java/com/spring_boot/practice/springtest/SpringTestApplication.java
@@ -0,0 +1,12 @@
+package com.spring_boot.practice.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+}
