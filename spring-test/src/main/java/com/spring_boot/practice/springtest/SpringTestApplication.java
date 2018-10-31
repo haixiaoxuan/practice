84e729f3724afb9f228e27c0da54dc5b184b473a
haixiaoxuan <15735185585@163.com>
2018-10-31 20:02:19 +0800

    spark

--- spring-test/src/main/java/com/spring_boot/practice/springtest/SpringTestApplication.java
+++ spring-test/src/main/java/com/spring_boot/practice/springtest/SpringTestApplication.java
@@ -2,11 +2,15 @@ package com.spring_boot.practice.springtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+//@ImportResource(locations = {"classpath:/bean.yml"})
 @SpringBootApplication
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringTestApplication.class, args);
+
 	}
 }
--- spring-test/src/test/java/com/spring_boot/practice/springtest/SpringTestApplicationTests.java
+++ spring-test/src/test/java/com/spring_boot/practice/springtest/SpringTestApplicationTests.java
@@ -1,7 +1,11 @@
 package com.spring_boot.practice.springtest;
 
+import com.spring_boot.practice.springtest.bean.Cat;
+import com.spring_boot.practice.springtest.bean.Dog;
+import com.spring_boot.practice.springtest.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringTestApplicationTests {
 
+//	@Autowired
+////	Person person;
+////	@Autowired
+////	Dog dog;
+	@Autowired
+	Cat cat;
 	@Test
 	public void contextLoads() {
+//		System.out.println(person.getName()+person.getAge());
+//		System.out.println(dog.getName()+dog.getAge());
+
+		System.out.println(cat.getName()+cat.getAge());
+
 	}
 
 }
